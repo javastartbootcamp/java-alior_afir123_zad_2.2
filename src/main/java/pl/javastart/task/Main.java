7dc6858ba31fc8fe963875ee6b77b33e63a02734
afir123 <kiri15@o2.pl>
2023-10-24 18:27:25 +0200

    zadanie3

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String product1 = "Mleko";
+        String producer1 = "Mlekowita";
+        double price1 = 2.5;
+
+        String product2 = "Czekolada";
+        String producer2 = "Wedel";
+        double price2 = 2.19;
+
+        System.out.println(product1 + " " + producer1 + " " + price1 + "zł");
+        System.out.println(product2 + " " + producer2 + " " + price2 + "zł");
+
+
     }
 }
